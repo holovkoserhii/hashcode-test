b7c78913a712163b05690367b22d46c8f92785f0
Serhii Holovko <sholovk@softserveinc.com>
2021-01-30 19:27:13 +0200

    11

---
+++ index.ts
@@ -0,0 +1,43 @@
+import serhiiFn from './players/serhii'
+import oleksandrFn from './players/oleksandr'
+import maksymFn from './players/maksym'
+
+/*
+String:
+`0 7 4 8
+7 4 1
+22`
+
+1)
+Transform to object: { lines: 3, averageNumber: 7, averageNumberPerLine: [5, 4, 22]}
+
+2) 
+Transform to multiple objects:
+[{averageNumberPerLine: 5, sumOfOtherTwo: 10, day: 'M'}, {averageNumberPerLine: 4, sumOfOtherTwo: 10, day: 'T'}, ...]
+
+3)
+Transform to multiple string lines:
+`M 15
+T 14
+...`
+*/
+
+type Days = 'M' | 'T' | 'W'
+
+interface player1out {
+    lines: number
+    averageNumber: number
+    averageNumberPerLine: number[]
+}
+
+interface player2out {
+    averageNumberPerLine: number
+    lisumOfOtherTwones: number
+    day: Days
+}[]
+
+interface player3out {
+// to string
+M: number
+// ...
+}
