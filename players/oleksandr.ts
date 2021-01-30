96d9e11e12e1a640525752b8e137920658e9ffbc
oleksandr <oleksandr_bereziuk1@epam.com>
2021-01-30 20:09:49 +0200

    task 3 function

--- players/oleksandr.ts
+++ players/oleksandr.ts
@@ -1,1 +1,16 @@
-export default () => {}
+import { player3out, player2out } from '../interfaces';
+
+const mockData: player2out = [
+    {averageNumberPerLine: 5, sumOfOtherTwo: 10, day: 'M'},
+    {averageNumberPerLine: 4, sumOfOtherTwo: 10, day: 'T'}
+];
+
+const handler = (inputData: player2out[]): player3out => {
+    const output: Partial<player3out> = {};
+    
+    inputData.forEach( item => output[item.day] = 1 );
+
+    return output as player3out;
+}
+
+export default handler;
