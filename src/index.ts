d09b81879d1780c14ab98bea89ff4a2212a4e171
Maksym <pryvolnev.m@gmail.com>
2021-02-06 23:14:27 +0200

    add fix for player2

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { function1 } from "./players/player1/player1";
 import { function2 } from "./players/player2/player2";
 import { function3 } from "./players/player3/player3";
 
-import "./players/player2/player2";
+// import "./players/player2/player2";
