58928394e0e491a694e643e77474848a3e241326
Serhii Holovko <holovko.s@gmail.com>
2021-02-10 19:54:38 +0200

    add import to index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { function1 } from "./players/player1/player1";
-import { function2 } from "./players/player2/player2";
-import { function3 } from "./players/player3/player3";
-
-// import "./players/player2/player2";
+import './players/player1/player1'
